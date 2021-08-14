e57c5fac76809b7807bec74a926c3551e909f183
abdul-rehman-2050 <supermind2002@gmail.com>
2021-08-14 14:11:43 +0500

    customer form updated

--- src/components/Customers/CustomerTabContact.vue
+++ src/components/Customers/CustomerTabContact.vue
@@ -1,5 +1,95 @@
 <template>
-    <div>
-        <h1>Contact info</h1>
-    </div>
+  <v-form v-model="valid">
+    <v-container class="pa-8">
+      <v-form ref="form" v-model="valid" :lazy-validation="lazy">
+        <v-row>
+          <v-col cols="12" md="6">
+            <v-text-field
+              v-model="firstname"
+              :rules="nameRules"
+              :counter="10"
+              label="First name"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="6">
+            <v-text-field
+              v-model="lastname"
+              :rules="nameRules"
+              :counter="10"
+              label="Last name"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="12">
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="12">
+            <v-select
+              color
+              v-model="hearaboutus"
+              :items="hearingOptions"
+              
+              label="How do you hear about us?"
+            ></v-select>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="validate"
+          >
+            Add New
+          </v-btn>
+
+          <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
+        </v-row>
+      </v-form>
+    </v-container>
+  </v-form>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    firstname: "",
+    lastname: "",
+    hearaboutus: "",
+    hearingOptions: [
+      "Facebook",
+      "Bing",
+      "Google Ads",
+      "Search Engine",
+      "Referals",
+    ],
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => v.length <= 10 || "Name must be less than 10 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+    ],
+  }),
+  methods: {
+    reset() {
+      this.$refs.form.reset();
+      this.valid = false;
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+  },
+};
+</script>
--- src/components/Customers/CustomerTabCustomFields.vue
+++ src/components/Customers/CustomerTabCustomFields.vue
@@ -1,19 +1,17 @@
 <template>
   <v-container fill-height>
-    <v-row v-for="(val,i) in items" :key="i" justify="center" align="center">
-      <v-col cols="12" sm="4">
-          {{val}}
-      </v-col>
-      <v-col cols="12" sm="4">
-          <v-text-field type="text" error-count="" placeholder="val" 
-          label="" append-icon=""  outlined color></v-text-field>
-          
-      </v-col>  
-
-    </v-row>  
     <v-row justify="center" align="center">
-      <v-col cols="12" sm="4">
-        <v-btn x-large elevation="3" color="primary" @click="addField">
+      <v-col cols="12" md="6" v-for="(val, i) in items" :key="i">
+        <v-text-field :label="val" required></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="12" md="4">
+          <v-text-field type="text" error-count="" placeholder="" 
+          label="attribute Name" append-icon="mdi-plus"  v-model="attributeName" outlined color></v-text-field>
+      </v-col>
+      <v-col cols="12" md="4">   
+        <v-btn large elevation="" color="primary" @click="addField">
           <v-icon>mdi-plus</v-icon>
         </v-btn>
       </v-col>
@@ -23,27 +21,28 @@
 
 <script>
 export default {
-    name: 'CustomerTabCustomFields',
-    components: {  },
+  name: "CustomerTabCustomFields",
+  components: {},
 
-    directives: {  },
+  directives: {},
 
-    data() {
-        return {
-          items:[
+  data() {
+    return {
+    
+      items: [],
+      attributeName: "",
+    };
+  },
 
-          ]  
-        };
+  mounted() {},
+
+  methods: {
+    addField: function () {
+       
+            this.items = [...this.items, this.attributeName];
+       
+      
     },
-
-    mounted() {
-        
-    },
-
-    methods: {
-        addField:function(){
-            this.items = [...this.items,"new item"];
-        }
-    },
+  },
 };
 </script>
